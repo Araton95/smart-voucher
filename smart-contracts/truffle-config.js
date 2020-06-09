5df800662bc9754f3cf4bfce7a6a3e9d2ef8e07d
Ararat Tonoyan <tonoyandeveloper@gmail.com>
2020-06-09 18:58:43 +0300

    Added POA truffle configs and env sample

--- smart-contracts/truffle-config.js
+++ smart-contracts/truffle-config.js
@@ -1,14 +1,25 @@
 require('dotenv').config()
 const HDWalletProvider = require('truffle-hdwallet-provider')
 
+// Seed of your account
+const mnemonic = process.env.DEPLOYER_MNEMONIC_PHASE
+const provider = process.env.POA_PRODUCTION_PROVIDER
+
 module.exports = {
   networks: {
-    development: {
+    "development": {
      host: "localhost",
      port: 8545,
      gasPrice: 0,
      network_id: "*"
-    }
+    },
+    "poa":  {
+      provider: new HDWalletProvider(mnemonic, provider, 0, 2),
+      network_id: 1,
+      gasLimit: 10000000,
+      gasPrice: 0,
+      network_id: '*'
+    },
   },
   mocha: {
     timeout: 100000
