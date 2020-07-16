50c35e49151881ff24987dd9277173ce80420109
Ararat Tonoyan <tonoyandeveloper@gmail.com>
2020-07-16 22:14:56 +0300

    Changed the signer address

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const SmartVoucher = artifacts.require('SmartVoucher.sol')
 
 module.exports = async (deployer, network, accounts) => {
     return deployer.deploy(SmartVoucher, accounts[0]).then(contractInstance => {
-        contractInstance.addSigner('0x9C16c04bDA8904EC30a0D7045b276a3c66012Fd3', { from: accounts[0] })
+        contractInstance.addSigner('0x378810509345707Ca128A31123f0e594fA9bf46c', { from: accounts[0] })
     })
 }
