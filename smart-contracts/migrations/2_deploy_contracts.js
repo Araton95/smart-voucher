5288dab5e5fe4d805c55d6b3adcbf297a2545721
Ararat Tonoyan <tonoyandeveloper@gmail.com>
2020-07-18 09:42:50 +0300

    Updated contract deployement method

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@ const SmartVoucher = artifacts.require('SmartVoucher.sol')
 
 module.exports = async (deployer, network, accounts) => {
     return deployer.deploy(SmartVoucher, accounts[0]).then(contractInstance => {
-        contractInstance.addSigner('0x378810509345707Ca128A31123f0e594fA9bf46c', { from: accounts[0] })
+        contractInstance.addSigner(process.env.SIGNER, { from: accounts[0] })
     })
 }
